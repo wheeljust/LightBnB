6e4085a484f46bd312b09353ccbcb95ded45e30b
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-15 03:57:03 +0000

    add event listeners for update and delete buttons

--- LightBnB_WebApp/public/javascript/components/property_listings.js
+++ LightBnB_WebApp/public/javascript/components/property_listings.js
@@ -25,10 +25,22 @@ $(() => {
     // check for user login
     getMyDetails()
     .then()
+
     for (const propertyId in properties) {
       const property = properties[propertyId];
       const listing = propertyListing.createListing(property, isReservation);
       addListing(listing);
+    }
+    
+    if (isReservation) {
+      $('.update-button').on('click', function() {
+        const idData = $(this).attr('id').substring(16);
+        console.log(`update ${idData}`);          
+      })
+      $('.delete-button').on('click', function() {
+        const idData = $(this).attr('id').substring(16);
+        console.log(`delete ${idData}`);          
+      })
     }
   }
   window.propertyListings.addProperties = addProperties;
