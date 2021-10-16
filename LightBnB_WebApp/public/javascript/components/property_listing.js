6bf65620221955f1f33ba7d1417f814ffb284c7a
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-16 19:17:37 +0000

    add class to the review button

--- LightBnB_WebApp/public/javascript/components/property_listing.js
+++ LightBnB_WebApp/public/javascript/components/property_listing.js
@@ -26,7 +26,7 @@ $(() => {
               ` : ``
             }
             ${(isReservation && !isReservation.upcoming) ? 
-              `<button id="add-review-${property.id}">Add a Review</button>` : ``
+              `<button id="add-review-${property.id}" class="add-review-button">Add a Review</button>` : ``
             } 
             ${!isReservation ? `<span id="review-details-${property.id}" class="review_details">
               Browse ${property.review_count} reviews
