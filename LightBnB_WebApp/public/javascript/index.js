25eaca504817641ac2e4b38834634d11dd5b5519
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-16 04:37:25 +0000

    add click listener for reviews

--- LightBnB_WebApp/public/javascript/index.js
+++ LightBnB_WebApp/public/javascript/index.js
@@ -4,9 +4,14 @@ $(() => {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
     
-    $('.reserve-button').on('click', function() {
+    $(document).on('click', '.reserve-button', function() {
       const idData = $(this).attr('id').substring(17);
       views_manager.show('newReservation', idData);
+    })
+
+    $(document).on('click', '.review_details', function() {
+      const idData = $(this).attr('id').substring(15);
+      console.log(idData);
     })
 
   });
