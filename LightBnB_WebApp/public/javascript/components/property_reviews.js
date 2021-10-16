fc82d5b03c37f95f56b9594b06c232941391a19a
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-16 05:20:27 +0000

    add property reviews view

---
+++ LightBnB_WebApp/public/javascript/components/property_reviews.js
@@ -0,0 +1,36 @@
+$(() => {
+  const $propertyReviews = $(`
+    <section class="property-reviews" id="property-reviews">
+      Loading...
+    </section>
+  `);
+
+  window.$propertyReviews = $propertyReviews;
+
+  // declaring an empty object to add methods onto
+  window.propertyReviews = {}
+
+  function clearReviews() {
+    $propertyReviews.empty();
+  }
+
+  window.propertyReviews.clearReviews = clearReviews;
+
+  function addReviews(reviews) {
+    clearReviews();
+    const reviewHtml = reviews.map(review => {
+      const { id, review_rating, review_text, name, start_date, end_date }  = review;
+      return `<article class="property-review">
+        <section class="property-review__details">
+          <h3>${review_rating}/5 stars</h3>
+          <p>Review done by: ${name}</p>
+          <p>Stay from ${moment.utc(start_date).format('ll')} to ${moment.utc(end_date).format('ll')}</p>
+          <p>${review_text}</p> 
+        </section>
+      </article>`
+    }).join('');
+    $propertyReviews.append(reviewHtml);
+  }
+
+  window.propertyReviews.addReviews = addReviews;
+})
