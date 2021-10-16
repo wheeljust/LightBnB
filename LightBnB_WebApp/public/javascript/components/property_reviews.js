a540cbbe7037392355e34faa5b5899f5366d5da5
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-16 05:31:53 +0000

    add more details to the property reviews page

--- LightBnB_WebApp/public/javascript/components/property_reviews.js
+++ LightBnB_WebApp/public/javascript/components/property_reviews.js
@@ -18,18 +18,25 @@ $(() => {
 
   function addReviews(reviews) {
     clearReviews();
+    $propertyReviews.append(`<h3>Reviews for ${reviews[0].property_title}</h3>`);
     const reviewHtml = reviews.map(review => {
       const { id, review_rating, review_text, name, start_date, end_date }  = review;
       return `<article class="property-review">
         <section class="property-review__details">
           <h3>${review_rating}/5 stars</h3>
           <p>Review done by: ${name}</p>
-          <p>Stay from ${moment.utc(start_date).format('ll')} to ${moment.utc(end_date).format('ll')}</p>
+          <p>Stay from ${moment(start_date).format('ll')} to ${moment(end_date).format('ll')}</p>
           <p>${review_text}</p> 
         </section>
       </article>`
     }).join('');
     $propertyReviews.append(reviewHtml);
+    $propertyReviews.append(`<span class="property-reviews__return" href="#">Return to Listings</span>`);
+  
+    $('.property-reviews__return').on('click', function() {
+      views_manager.show('none');
+      views_manager.show('listings');
+    })
   }
 
   window.propertyReviews.addReviews = addReviews;
